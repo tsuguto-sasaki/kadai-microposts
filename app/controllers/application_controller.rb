e1f3da6ac17d14d6f2f89076689b5aa8f73fb5f0
tsuguto.sasaki <sasaki.tsuguto0613@gmail.com>
2019-11-23 13:02:56 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ class ApplicationController < ActionController::Base
 
   def require_user_logged_in
     unless logged_in?
-      redirect_to login_url
+      redirect_to 
     end
   end
   
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(10)
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
@@ -16,7 +17,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -25,10 +26,22 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
 
-private
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
